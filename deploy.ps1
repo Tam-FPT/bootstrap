1ed17b4971a55c7486e3e1a2b80147adba79a51a
Tran Thien Tam <11405524+Tam-FPT@users.noreply.github.com>
2019-11-15 17:09:17 -0800

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
-robocopy config.json d:\config.json
-stop-service CPECoreManager
+echo "hello world"
+#stop-service CPECoreManager
 ping 127.0.0.1 -n 5
-robocopy \\10.0.5.80\cpeprod\publish C:\CPEPROD\PUBLISH /E /MT:24 /XD logs
-start-service CPECoreManager
+#robocopy \\10.0.5.80\cpeprod\publish C:\CPEPROD\PUBLISH /E /MT:24 /XD logs
+#start-service CPECoreManager
