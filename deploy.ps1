62a3d4f34c40ca395d0829f4cf32b1ab55a26986
Tran Thien Tam <11405524+Tam-FPT@users.noreply.github.com>
2019-11-15 21:25:39 -0800

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,3 @@
-echo "hello world"
-stop-service CPECoreManager
-ping 127.0.0.1 -n 5
-robocopy \\10.0.5.80\cpeprod\publish C:\CPEPROD\PUBLISH /E /MT:24 /XD logs
-start-service CPECoreManager
+robocopy \\10.0.5.80\cpeprod\publish\ocrcore C:\CPEPROD\PUBLISH\ocrcore /E /MT:24 /XD logs
+cd C:\CPEPROD\PUBLISH\ocrcore
+preprod
